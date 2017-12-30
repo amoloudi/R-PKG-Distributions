fdf7e8f641b96e7dbe2ab7564b118bb4d45b7088
amoloudi <mohammadaminmoloudi12369@gmail.com>
2017-12-30 13:13:39 +0330

    Fix bigen

--- R/bigenV.R
+++ R/bigenV.R
@@ -1,12 +1,12 @@
 bigenV <- function(N, p, n) {
-  generated = brgenV(N, p)
+  generated = brgenV(N * n, p)
   counter = 0
   output = numeric()
-  for(i in 1:N){
+  for(i in 1:(N * n)){
     if(generated[i] == 1){
       counter = counter + 1
     }
-    if((i %% n) == 0){
+    if((i %% N) == 0){
       output = c(output, counter)
       counter = 0
     }
